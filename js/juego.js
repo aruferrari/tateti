4f8af74e40f81e9b5eee136e16300d1ffc496ddf
Adrian <Adrin Sarabia>
2017-05-04 17:46:01 -0300

    Subo clase juego

---
+++ js/juego.js
@@ -0,0 +1,19 @@
+  function Juego()
+  {
+    var participante1;
+    var participante2;
+    var tablero;
+    var turno;
+  }
+
+  function dibujarTablero()
+  {
+    
+  }
+
+  function verificarGanador(participante1, panticipante2)
+  {
+      
+  }
+
+
