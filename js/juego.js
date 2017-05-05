3e26d709d25b10d3e5d6d45e44811b1c3f396da3
sergio ponce <sergio ponce>
2017-05-04 22:30:11 -0300

    version 0.1

--- js/juego.js
+++ js/juego.js
@@ -1,22 +1,29 @@
-  function Juego()
-  {
-    var participante1;
-    var participante2;
-    var tablero;
-    var turno;
-  }
-
-  function dibujarTablero()
-  {
+class Juego {
     
-  }
-
-  function verificarGanador(participante1, panticipante2)
-  {
-      
-  }
-function setTurno(){
+    constructor(nombre1, nombre2, estado){
+        this.tablero = { 1 : " ", 2 : " ", 3 : " ", 4 : " ", 5 : " ", 6 : " ", 7 : " ", 8 : " ", 9 : " " };
+        this.jugador = [nombre1, nombre2];
+        this.turno = 0;// turno guardaria el valor del jugador actual
+        this.estado = estado;
+        this.criterioDeGanador = ["123" ,"456" ,"789" ,"147" ,"258" ,"369" ,"159" ,"357"];
+    }
     
+    verificarSiHayGanador(){
+        for(let i of this.criterioDeGanador){
+            let f = i.split("");//Esto tranforma "123" en un array [1, 2, 3]
+            if(this.tablero[parseInt(f[0])] === this.turno && this.tablero[parseInt(f[1])] === this.turno && this.tablero[parseInt(f[2])] === this.turno){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    setTurno(){
+        if(this.turno == 0){
+            this.turno = 1
+        }else{
+            this.turno = 0
+        }
+        
+    }
 }
-
-
--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 console.log("Bienvenidos a Tateti");
-var juego = new Juego();
+var jugador1 = prompt("Nombre Jugador 1", "Pepe");
+var jugador2 = prompt("Nombre Jugador 1", "Lalo");
+
+var juego = new Juego(jugador1, jugador2, "jugando");
 
 do {
-    var casilla =  prompt("Ingrese una casilla");
-    juego.tablero = juego.turno
-    juego.dibujarTableroEnConsola();
-    if (juego.verificarSiHayGanador(juego.turno)){
+    var casilla =  prompt("Turno de " + juego.jugador[juego.turno]);
+    juego.tablero[casilla] = juego.turno
+    console.log(juego.tablero);
+    if (juego.verificarSiHayGanador()){
         juego.estado = "finalizado";
     }else{
         juego.setTurno();
     }
     
-} while(juego.estado != "jugando");
+} while(juego.estado == "jugando");
+
+console.log("El ganador es " + juego.jugador[juego.turno]);
 
